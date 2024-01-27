1d0ea0fd1efbc986b1d3fbcea358e70618b35d93
Karthikraut <karthikraut2@gmail.com>
2024-01-27 13:35:05 +0530

    Commited

--- 1_Setting Up NodeJs Server/server.js
+++ 1_Setting Up NodeJs Server/server.js
@@ -4,6 +4,7 @@ const  server  = http.createServer(function(req,res){
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.write('<html><body><p>This is my Home Page</p> </body> </html>');
         res.end();
+        
     }
     else if(req.url == "/about"){
         res.writeHead(200, {'Content-Type': 'text/html'});
